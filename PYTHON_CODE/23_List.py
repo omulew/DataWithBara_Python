1bd9f7a1341f6b2d43940a637df6c48052e5b179
Krystian Przybyłek <krystianprzybylek@gmail.com>
2026-03-06 19:30:02 +0100

    Elsnera  2123

---
+++ PYTHON_CODE/23_List.py
@@ -0,0 +1,32 @@
+
+
+empty = []
+letters = ['a', 'b', 'c']
+numbers = [1, 2, 3, 4]
+mixed = [1, 'a', None, True]
+# print(empty)
+# print(type(empty))
+# print(type(letters))
+# print(type(numbers))
+# print(type(mixed))
+
+# empty = list()
+# print(empty)
+
+# letters = list('Python')
+# print(letters)
+
+# numbers = list(range(5))
+# print(numbers)
+
+matrix = [['a', 'b', 'c'],
+          ['d', 'e', 'f']]
+
+matrix_mixed = [['a', 'b', 'c'],
+                [1, 2, 3, 4]]
+
+print(matrix)
+print(type(matrix))
+
+print(matrix_mixed)
+print(type(matrix_mixed))
