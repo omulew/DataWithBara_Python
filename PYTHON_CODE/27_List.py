1f101150ae5d88b5f10411edded9e12aacb250ad
Krystian Przybyłek <krystianprzybylek@gmail.com>
2026-03-26 19:30:07 +0100

    Lekcja 27

---
+++ PYTHON_CODE/27_List.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 5, 6]
+
+# numbers.append('A')
+numbers.insert(1, 'd')
+numbers.remove(2)
+numbers.pop(3)
+numbers.pop()
+print(numbers)
+
+matrix = [['a', 'b', 'c'],
+          ['d', 'e', 'f']]
+
+matrix.append(['g', 'h', 'i'])
+matrix[1].append(['g'])
+matrix[1].append('X')
+# print(matrix)
